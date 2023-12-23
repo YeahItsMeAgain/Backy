bb45179e243353a230451dff9e1f7ae2d1dbf064
Mevaser <mevaser.rotner@gmail.com>
2023-12-23 22:34:29 +0200

    new logger

---
+++ src/logger.rs
@@ -0,0 +1,30 @@
+use log::{Level, LevelFilter};
+
+use env_logger::{
+    fmt::{Color, Style, StyledValue},
+    Builder,
+};
+
+fn colored_level<'a>(style: &'a mut Style, level: Level) -> StyledValue<'a, &'static str> {
+    match level {
+        Level::Trace => style.set_color(Color::Magenta).value("TRACE"),
+        Level::Debug => style.set_color(Color::Blue).value("DEBUG"),
+        Level::Info => style.set_color(Color::Green).value("INFO "),
+        Level::Warn => style.set_color(Color::Yellow).value("WARN "),
+        Level::Error => style.set_color(Color::Red).value("ERROR"),
+    }
+}
+
+pub fn init() {
+    let mut builder = Builder::new();
+
+    builder.format(|f, record| {
+        use std::io::Write;
+
+        let mut style = f.style();
+        let level = colored_level(&mut style, record.level());
+        writeln!(f, "{}> {}", level, record.args())
+    });
+
+    builder.filter_level(LevelFilter::Info).init();
+}
