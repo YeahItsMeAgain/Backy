cf962400440a34a8a1122bdeb5d1437680a2adc2
Mevaser <mevaser.rotner@gmail.com>
2023-12-30 04:13:31 +0200

    small refactor

--- src/opts/vault.rs
+++ src/opts/vault.rs
@@ -11,6 +11,23 @@ use generator::{done, Generator, Gn};
 use std::path::Path;
 use walkdir::WalkDir;
 
+fn run_command(command: &str) -> Result<()> {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .current_dir(&config::get().vault)
+        .stdout(Stdio::null())
+        .stderr(Stdio::piped())
+        .output()
+        .context(format!("Failed to run command: {:?}", command))?;
+
+    if !output.stderr.is_empty() {
+        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    Ok(())
+}
+
 pub fn init() -> Result<()> {
     if Path::join(Path::new(&config::get().vault), ".git").exists() {
         return Ok(());
@@ -21,56 +38,25 @@ pub fn init() -> Result<()> {
         config::get().vault
     ))?;
 
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
-            "git init && git remote add origin {} && git lfs install",
-            config::get().repo
-        ))
-        .current_dir(&config::get().vault)
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .output()
+    run_command("git init && git remote add origin {} && git lfs install")
         .context("Failed to init repo")?;
-
-    if !output.stderr.is_empty() {
-        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
-    }
     Ok(())
 }
 
 pub fn push() -> Result<()> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
+    run_command(
+        format!(
             "git add .; git commit -m \"{}\" -q; git push origin master -q",
             chrono::offset::Local::now()
-        ))
-        .current_dir(&config::get().vault)
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .output()
-        .context("Failed to push changes")?;
-
-    if !output.stderr.is_empty() {
-        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
-    }
+        )
+        .as_str(),
+    )
+    .context("Failed to push changes")?;
     Ok(())
 }
 
 pub fn pull() -> Result<()> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg("git pull origin master -q")
-        .current_dir(&config::get().vault)
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .output()
-        .context("Failed to pull changes")?;
-
-    if !output.stderr.is_empty() {
-        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
-    }
+    run_command("git pull origin master -q").context("Failed to pull changes")?;
     Ok(())
 }
 
@@ -93,64 +79,43 @@ pub fn list() -> Result<Generator<'static, (), PathBuf>> {
 }
 
 fn track_file(path: String) -> Result<()> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
+    run_command(
+        format!(
             "git lfs track \"*.{}\"; git add .gitattributes; git commit -m \"Updated .gitattributes\"",
             Path::new(&path).extension().unwrap().to_str().unwrap()
-        ))
-        .current_dir(&config::get().vault)
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .output()
-        .context("Failed to track new file")?;
-
-    if !output.stderr.is_empty() {
-        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
-    }
+        )
+        .as_str(),
+    )
+    .context("Failed to track new file")?;
     Ok(())
 }
 
 pub fn add(path: String) -> Result<()> {
     track_file(path.clone())?;
 
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
+    run_command(
+        format!(
             "git add {} && git commit -m \"Added {} - {}\" -q",
             path,
             path,
             chrono::offset::Local::now()
-        ))
-        .current_dir(&config::get().vault)
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .output()
-        .context("Failed to push changes")?;
-
-    if !output.stderr.is_empty() {
-        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
-    }
+        )
+        .as_str(),
+    )
+    .context("Failed to push changes")?;
     Ok(())
 }
 
 pub fn rm(path: String) -> Result<()> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
+    run_command(
+        format!(
             "git rm {} && git commit -m \"Removed {} - {}\" -q",
             path,
             path,
             chrono::offset::Local::now()
-        ))
-        .current_dir(&config::get().vault)
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .output()
-        .context("Failed to push changes")?;
-
-    if !output.stderr.is_empty() {
-        log::warn!("{}", String::from_utf8_lossy(&output.stderr));
-    }
+        )
+        .as_str(),
+    )
+    .context(format!("Failed to remove {}", path))?;
     Ok(())
 }
